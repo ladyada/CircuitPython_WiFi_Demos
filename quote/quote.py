6705637deace5bcbd75dc2d51e52c9bb01e8ad5a
ladyada <limor@ladyada.net>
2019-02-10 15:33:07 -0500

    update quotes

--- quote/quote.py
+++ quote/quote.py
@@ -1,74 +1,33 @@
 import time
 import board
-import adafruit_pyportal
+from adafruit_pyportal import PyPortal
 from adafruit_bitmap_font import bitmap_font
 from adafruit_display_text.text_area import TextArea
 
 # Set up where we'll be fetching data from
 DATA_SOURCE = "https://www.adafruit.com/api/quotes.php"
-DATA_LOCATION = [0]
+QUOTE_LOCATION = [0, 'text']
+AUTHOR_LOCATION = [0, 'author']
 
 cwd = __file__.rsplit('/', 1)[0]
-pyportal = adafruit_pyportal.PyPortal(url=DATA_SOURCE, json_path=DATA_LOCATION,
-                           status_neopixel=board.NEOPIXEL,
-                           default_bg=cwd+"/quote_background.bmp")
-
-quote_font = bitmap_font.load_font("/fonts/Arial-ItalicMT-17.bdf")
-quote_font.load_glyphs(b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
-quote_position = (10, 30)
-quote_color = 0xFFFFFF
-author_font = quote_font
-author_position = (150, 180)
-author_color = 0x0000FF
-
-# return a list of lines with wordwrapping
-def wrap_nicely(string, max_chars):
-    words = string.split(' ')
-    the_lines = []
-    the_line = ""
-    for w in words:
-        if len(the_line+' '+w) <= max_chars:
-            the_line += ' '+w
-        else:
-            the_lines.append(the_line)
-            the_line = ''+w
-    if the_line:      # last line remaining
-        the_lines.append(the_line)
-    return the_lines
-
-value = {'text': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard', 'author': 'Harriet Tubman'}
-quote_text = None
-author_text = None
-def draw_text(quote, author):
-    global quote_text, author_text
-    if quote_text:
-        pyportal.splash.pop()
-    if author_text:
-        pyportal.splash.pop()
-    quote = '\n'.join(wrap_nicely(quote, 38))
-    quote_text = TextArea(quote_font, text=quote)
-    quote_text.color = quote_color
-    quote_text.x = quote_position[0]
-    quote_text.y = quote_position[1]
-    pyportal.splash.append(quote_text.group)
-
-    author_text = TextArea(author_font, text=author)
-    author_text.color = author_color
-    author_text.x = author_position[0]
-    author_text.y = author_position[1]
-    pyportal.splash.append(author_text.group)
-
-    board.DISPLAY.wait_for_frame()
-
-draw_text(value['text'], value['author'])
+pyportal = PyPortal(url=DATA_SOURCE,
+                    json_path=(QUOTE_LOCATION, AUTHOR_LOCATION),
+                    status_neopixel=board.NEOPIXEL,
+                    default_bg=cwd+"/quote_background.bmp",
+                    text_font="/fonts/Arial-ItalicMT-17.bdf",
+                    text_position=((20, 40),  # quote location
+                                   (5, 190)), # author location
+                    text_color=(0xFFFFFF,  # quote text color
+                                0x8080FF), # author text color
+                    text_wrap=(40, # characters to wrap for quote
+                               0), # no wrap for author
+                    text_maxlen=(180, 30) # max text size
+)
 
 while True:
     try:
         value = pyportal.fetch()
         print("Response is", value)
-        draw_text(value['text'], value['author'])
     except RuntimeError as e:
         print("Some error occured, retrying! -", e)
-    except ValueError as e:
-        print("Couldn't parse JSON - ", e)
-    time.sleep(5)
+    time.sleep(60)
