67c3da72f8dcbb292317629ebef43e7868abdc90
Marty McGuire <schmartissimo@gmail.com>
2019-02-14 09:11:44 -0500

    Thingiverse tweaks
    * display public URL rather than API URL
    * fix occasional index error (randint is inclusive)
    * use display_large so images aren't stretched
    * sleep so we're not hitting the API so much

--- thingiverse/thingiverse.py
+++ thingiverse/thingiverse.py
@@ -15,7 +15,7 @@ NUM_THINGS=25  # how many things to select from (we randomize between em)
 DATA_SOURCE = "https://api.thingiverse.com/users/adafruit/things?per_page="+str(NUM_THINGS)+"&access_token=" + settings['thingiverse_token']
 IMAGE_LOCATION = [0, "thumbnail"]
 TITLE_LOCATION = [0, "name"]
-URL_LOCATION = [0, "url"]
+URL_LOCATION = [0, "public_url"]
 
 cwd = __file__.rsplit('/', 1)[0]
 pyportal = adafruit_pyportal.PyPortal(url=DATA_SOURCE,
@@ -23,8 +23,8 @@ pyportal = adafruit_pyportal.PyPortal(url=DATA_SOURCE,
                                       status_neopixel=board.NEOPIXEL,
                                       default_bg=cwd+"/thingiverse_background.bmp",
                                       text_font="/fonts/Arial.bdf",
-                                      text_position=((5, 10), (5, 200), None),
-                                      text_color=(0xFFFFFF, 0xFFFFFF, None),
+                                      text_position=((5, 10), (5, 200)),
+                                      text_color=(0xFFFFFF, 0xFFFFFF),
                                       text_maxlen=(50, 50, None), # cut off characters
                                       debug=False)
 
@@ -34,7 +34,7 @@ while True:
         pyportal.set_background(None)
         response = pyportal.fetch()
         print("Response is", response)
-        image_url = response[2].replace('_thumb_medium.jpg', '_display_medium.jpg')
+        image_url = response[2].replace('_thumb_medium.', '_display_large.')
         pyportal.wget(adafruit_pyportal.IMAGE_CONVERTER_SERVICE+image_url, "/cache.bmp")
         pyportal.set_background("/cache.bmp")
 
@@ -42,7 +42,7 @@ while True:
         print("Some error occured, retrying! -", e)
 
     # next thingy should be random!
-    thingy = random.randint(0, NUM_THINGS)
+    thingy = random.randint(0, NUM_THINGS - 1)
     URL_LOCATION[0] = TITLE_LOCATION[0] = IMAGE_LOCATION[0] = thingy
 
-    time.sleep(0)
+    time.sleep(30)
